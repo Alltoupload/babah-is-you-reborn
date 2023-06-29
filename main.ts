7d3bccc2ca8efa4bc74cd3a74d72d836bf7c2064
Alltoupload <101133556+Alltoupload@users.noreply.github.com>
2023-06-29 15:12:57 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,18 @@
+controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
+    Ticks += 1
+    music.play(music.createSoundEffect(WaveShape.Sine, 1, 1483, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
+    Babah.x += controller.dx(1600)
+    Babah.y += controller.dy(1600)
+})
+function add_rule (ex_babah: string, be: string, ex_you: string) {
+    rules = [[ex_babah, be, ex_you]]
+}
+let rules: string[][] = []
+let Babah: Sprite = null
+let list: string[] = []
+console.logValue("list[\"aaa\"]", list.indexOf("aaa"))
+let Ticks = 0
 music.play(music.createSong(assets.song`Theme`), music.PlaybackMode.LoopingInBackground)
-let Babah = sprites.create(assets.image`Babah`, SpriteKind.Player)
+Babah = sprites.create(assets.image`Babah`, SpriteKind.Player)
+Babah.scale = 2
+add_rule("Babah", "Be", "You")
