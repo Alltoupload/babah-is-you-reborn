e4f358a092d5549e1016793a0f287edd44453db8
Alltoupload <101133556+Alltoupload@users.noreply.github.com>
2023-06-29 16:47:31 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,15 +4,11 @@ controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
     Babah.x += controller.dx(1600)
     Babah.y += controller.dy(1600)
 })
-function add_rule (ex_babah: string, be: string, ex_you: string) {
-    rules = [[ex_babah, be, ex_you]]
-}
-let rules: string[][] = []
 let Babah: Sprite = null
 let list: string[] = []
 console.logValue("list[\"aaa\"]", list.indexOf("aaa"))
-let Ticks = 0
-music.play(music.createSong(assets.song`Theme`), music.PlaybackMode.LoopingInBackground)
 Babah = sprites.create(assets.image`Babah`, SpriteKind.Player)
+let Ticks = 0
+Babah.setStayInScreen(true)
+music.play(music.createSong(assets.song`Theme`), music.PlaybackMode.LoopingInBackground)
 Babah.scale = 2
-add_rule("Babah", "Be", "You")
